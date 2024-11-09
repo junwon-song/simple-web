d977b5939663d4dc97bead27e0dfd49577c1c78f
junwon-song <thdqls12@gmail.com>
2024-11-09 16:05:29 +0900

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Starting the script"
 if [ -d /usr/share/nginx/html ]; then
-    sudo rm -rf /usr/share/nginx/html/*
+    echo "Directory exists, proceeding with cleanup"
+    rm -rf /usr/share/nginx/html/*
+else
+    echo "Directory does not exist"
 fi
+echo "Script finished"
